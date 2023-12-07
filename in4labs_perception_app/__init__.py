4ffba07272331a2428af4d8d1d5ebead86d2e034
cRejon <carlosrejongomez@gmail.com>
2023-12-07 14:13:25 +0100

    Add perception app

---
+++ in4labs_perception_app/__init__.py
@@ -0,0 +1,217 @@
+import os
+import subprocess
+import time
+
+import pexpect
+import requests
+from flask import Flask, render_template, url_for, jsonify, redirect, send_file, flash, request
+from flask_login import LoginManager, UserMixin, login_required, current_user, login_user
+
+from .utils import get_usb_config, create_editor, create_navtab
+
+# Test environment variables
+from datetime import datetime, timedelta
+end_time = datetime.now() + timedelta(minutes=15)
+os.environ['USER_EMAIL'] = 'admin@email.com'
+os.environ['USER_ID'] = '1'
+os.environ['END_TIME'] = end_time.strftime('%Y-%m-%dT%H:%M:%S')
+os.environ['CAM_URL'] = 'http://62.204.201.51:8100/Mjpeg/1?authToken=2454ef16-84cf-4184-a748-8bddd993c078'
+
+
+# Flask environment variable needed for session management
+flask_config = {
+    # Use as secret key the user email + the end time of the session 
+    'SECRET_KEY': os.environ.get('USER_EMAIL') + os.environ.get('END_TIME'),
+}
+
+# Docker environment variables
+cam_url = os.environ.get('CAM_URL') 
+user_email = os.environ.get('USER_EMAIL') 
+end_time = os.environ.get('END_TIME') 
+
+# Boards configuration
+boards = {
+    'Board_1':{
+        'name':'Mega 2560',
+        'model':'Arduino Mega 2560 Rev3',
+        'fqbn':'arduino:avr:mega',
+        'usb_port':'3',
+    }
+}
+
+boards = get_usb_config(boards) # Get the serial number and driver of the boards
+
+app = Flask(__name__, instance_path=os.path.join(os.getcwd(), 'arduino'))
+app.config.from_mapping(flask_config)
+
+# Create the subfolders for the compilations
+try:
+    for board in boards.keys():
+        os.makedirs(os.path.join(app.instance_path, 'compilations', board))
+        for dir in ['build', 'cache', 'temp_sketch']:
+            os.makedirs(os.path.join(app.instance_path, 'compilations', board, dir))
+except OSError:
+    pass
+
+# Flask-Login configuration
+login_manager = LoginManager()
+login_manager.login_view = 'login' # Set the default login page
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return user
+
+class User(UserMixin):
+    def __init__(self, id, email):
+        self.id = id
+        self.email = email
+
+user = User(id=1, email=user_email)
+
+
+# Flask routes
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index')) 
+
+    if request.method == 'POST':
+        email = request.form['email']
+        if email == user_email:
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid email address. Please try again.')
+    
+    return render_template('login.html')
+
+@app.route('/index')
+@login_required
+def index():
+    navtabs = []
+    editors = []
+    for board in boards.items():
+        navtabs.append(create_navtab(board))
+        editors.append(create_editor(board))
+    return render_template('index.html', boards=boards, navtabs=navtabs,
+                                editors=editors, cam_url=cam_url, end_time=end_time)
+
+@app.route('/get_example', methods=['GET'])
+@login_required
+def get_example(): 
+    example = request.args.get('example')      
+    examples_path = os.path.join(app.instance_path, 'examples')
+
+    # Find example file in the corresponding folder
+    for folder in os.listdir(examples_path):
+        if example in os.listdir(os.path.join(examples_path, folder)):
+            example_file = os.path.join(examples_path, folder, example)
+            break
+
+    return send_file(example_file, mimetype='text')
+
+@app.route('/compile', methods=['POST'])
+@login_required
+def compile():
+    board = request.form['board']
+    code = request.form['text']
+
+    compilation_path = os.path.join(app.instance_path, 'compilations', board)
+    sketch_path = os.path.join(compilation_path, 'temp_sketch')
+
+    with open(os.path.join(sketch_path, 'temp_sketch.ino'), 'w') as f:
+        f.write(code)
+
+    command = ['arduino-cli', 'compile', '--fqbn', boards[board]['fqbn'],
+    '--build-cache-path', os.path.join(compilation_path, 'cache'), 
+    '--build-path', os.path.join(compilation_path, 'build'), 
+    sketch_path]
+
+    result = subprocess.run(command, capture_output=True, text=True) 
+
+    resp = jsonify(board=board, error=result.stderr)
+    return resp
+
+@app.route('/execute', methods=['POST'])
+@login_required
+def execute():
+    board = request.form['board']
+    target = request.form['target']
+
+    upload_sketch(board, target)
+
+    resp = jsonify(board=board)
+    return resp
+
+def upload_sketch(board, target):
+    if (target == 'user'): 
+        input_file = os.path.join(app.instance_path, 'compilations', board, 'build','temp_sketch.ino.hex')
+    else: # target == 'stop'
+        input_file = os.path.join(app.instance_path, 'compilations', 'precompiled','stop.ino.hex')
+
+    fqbn = boards[board]['fqbn']
+    usb_interface = boards[board]['usb_interface']
+
+    command = ['arduino-cli', 'upload', '--fqbn', fqbn,
+                '--port', f'/dev/{usb_interface}', '--input-file', input_file]
+    
+    result = subprocess.run(command, capture_output=True, text=True) 
+    print(result) # Debug info
+
+
+@app.route('/monitor', methods=['GET'])
+@login_required
+def monitor():
+    board = request.args.get('board')
+    baudrate = request.args.get('baudrate', default=9600, type=int)
+    seconds = request.args.get('seconds', default=10, type=int)
+
+    usb_interface = boards[board]['usb_interface']
+
+    command = f'arduino-cli monitor -p /dev/{usb_interface} --quiet --config baudrate={baudrate}'
+    # NOTE: pexpect is used because arduino-cli monitor expects to run in an interactive 
+    #       terminal environment and subprocess.run() does not work properly.
+    child = pexpect.spawn(command)
+    
+    try:
+        child.expect(pexpect.EOF, timeout=seconds)
+    except pexpect.TIMEOUT:
+        pass
+
+    output = child.before.decode('utf-8')
+    
+    child.close()
+        
+    resp = jsonify(board=board, output=output)
+    return resp
+
+@app.route('/suggest', methods=['POST'])
+@login_required
+def suggest():
+    board = request.form['board']
+    code = request.form['text']
+
+    params = {'action': '16', 'text': code}
+    url = 'https://open.ieec.uned.es/v_innovacion/api.php'
+
+    r = requests.get(url, params=params)
+    suggestion = r.text
+
+    resp = jsonify(board=board, suggestion=suggestion)
+    return resp
+
+@app.route('/reset_lab', methods=['GET'])
+@login_required
+def reset():
+    # Uhubctl is used to power on/off the USB ports of the Raspberry Pi
+    command = ['uhubctl', '-a', 'cycle', '-l', '1-1', '-d', '2']
+    result = subprocess.run(command, capture_output=True, text=True)
+    # Load the stop code in all the boards
+    time.sleep(1)
+    for board in boards:
+        upload_sketch(board, 'stop')
+    # Return the output of the command for debugging purposes
+    resp = jsonify(result=result.stdout)
+    return resp
+
---
+++ in4labs_perception_app/utils.py
@@ -0,0 +1,160 @@
+import os
+import subprocess
+import re
+
+from flask import current_app
+
+
+def get_usb_config(boards):
+    result = subprocess.run(['dmesg'], capture_output=True, text=True)
+    dmesg_output = result.stdout
+
+    pattern_usb_interface = r'1-1.(\d).*?(tty\w\w\w\d)'
+    pattern_serial_number = r'1-1.(\d): SerialNumber:\s(.*?)\n'
+    matches_interface = re.findall(pattern_usb_interface, dmesg_output)
+    matches_serial_number = re.findall(pattern_serial_number, dmesg_output)
+
+    matches = []
+    for match_interface in matches_interface:
+        for match_serial_number in matches_serial_number:
+            if match_interface[0] == match_serial_number[0]:
+                matches.append(match_interface + match_serial_number[1:])
+                break
+
+    for match in matches:
+        for board in boards.values():
+            if board['usb_port'] == match[0]:
+                board['usb_interface'] = match[1]
+                board['serial_number'] = match[2]
+                break
+
+    return boards
+
+def fill_examples(board):
+    key = board[0]
+
+    example_path = os.path.join(current_app.instance_path, 'examples', key)
+
+    examples = []
+    examples += [file for file in os.listdir(example_path) if file.endswith('.ino')]
+    # Sort them alphabetically
+    examples.sort()
+
+    # Convert example names to show spaces instead of underscores and remove .ino extension 
+    example_names = [example.replace('_', ' ').replace('.ino', '') for example in examples]
+
+    examples_html = ''
+    for example, example_name in zip(examples, example_names):
+        if example_name == 'New Sketch':
+            examples_html += f'<option value="{example}" selected="selected">{example_name}</option>\n'
+        else:
+            examples_html += f'<option value="{example}">{example_name}</option>\n'
+    
+    return examples_html
+
+def create_navtab(board):
+    key = board[0]
+    name = board[1]['name']
+    navtab_html = f'''
+        <button class="nav-link {name.lower()} col-sm-4" id="nav-{key}-tab" data-bs-toggle="tab" data-bs-target="#nav-{key}" type="button" role="tab" aria-controls="nav-{key}" aria-selected="true">{name}</button>
+    '''
+
+    return navtab_html
+
+def create_editor(board):
+    key = board[0]
+
+    examples = fill_examples(board)
+
+    editor_html = f'''
+                <div class="tab-pane fade active show" id="nav-{key}" role="tabpanel" aria-labelledby="nav-{key}-tab">
+                    <div class="editor-nav">
+                        <div class="row">
+                            <div class="editor-examples col-sm-4">
+                                <div class="btn-group editor-examples-dropdown">
+                                    <select class="editor-select" id="editor-select-{key}" onchange="onLoadExample('{key}',this.value)">
+                                        {examples}
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="editor-cta col-sm-8">
+                                <div class="editor-cta-load">
+                                    <button class="upload" onclick="document.getElementById('file-input-{key}').click()" data-toggle="tooltip" data-placement="top" title="Load File"><span class="fa fa-upload"/></button>
+                                    <input id="file-input-{key}" type="file" accept=".ino" style="display: none;" />
+                                    <script>
+                                        document.getElementById('file-input-{key}').addEventListener('change', onLoadFile, false);
+                                    </script>
+                                    <button class="download" onclick="onSaveFile('{key}')" data-toggle="tooltip" data-placement="top" title="Save File"><span class="fa fa-download"/></button>
+                                    <button class="suggest"
+                                            id="button-suggest-{key}"
+                                            onclick="onSuggest('{key}')"
+                                            data-toggle="tooltip"
+                                            data-placement="top"
+                                            title="Suggest">
+                                        <span class="fa fa-commenting"/>
+                                    </button>
+                                </div>
+                                <div class="editor-cta-compile">
+                                    <button class="compile"
+                                            onclick="onCompileCode('{key}')"
+                                            data-toggle="tooltip"
+                                            data-placement="top"
+                                            title="Compile code">
+                                        <span class="fa fa-check"/>
+                                    </button>
+                                    <button class="execute"
+                                            id="button-execute-{key}"
+                                            onclick="onExecuteCode('{key}')"
+                                            data-toggle="tooltip"
+                                            data-placement="top"
+                                            title="Run"
+                                            disabled="disabled">
+                                        <span class="fa fa-play-circle"/>
+                                    </button>
+                                    <button class="monitor"
+                                            id="button-monitor-{key}"
+                                            onclick="setupMonitor('{key}')"
+                                            data-toggle="tooltip"
+                                            data-placement="top"
+                                            title="Monitor"
+                                            disabled="disabled">
+                                        <span class="fa fa-terminal"/>
+                                    </button>
+                                    <button class="stop"
+                                            id="button-stop-{key}"
+                                            onclick="onStopExecution('{key}')"
+                                            data-toggle="tooltip"
+                                            data-placement="top"
+                                            title="Stop"
+                                            disabled="disabled">
+                                        <span class="fa fa-stop-circle"/>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <form id="editor-{key}">
+                        <textarea id="text-{key}" name="text-{key}"></textarea>
+                        <script>
+                            let editor_{key} = CodeMirror.fromTextArea(document.getElementById('text-{key}'), {{
+                                mode: 'text/x-c++src',
+                                theme: 'neat',
+                                lineNumbers: true,
+                                autoCloseBrackets:true,
+                            }});
+
+                            function {key}GetEditor() {{
+                                return editor_{key};
+                            }}
+
+                            onLoadExample('{key}','New_Sketch.ino');
+                            onStopExecution('{key}');
+                            
+                            // Listener to trigger a function when the code changes
+                            editor_{key}.on("change", function() {{ onChangeCode('{key}') }});
+                        </script>
+                    </form>
+                </div>
+    '''
+
+    return editor_html
