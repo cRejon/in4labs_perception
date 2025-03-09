3175aa4517a7300c0fe9498094cf7848815dd047
cRejon <carlosrejongomez@gmail.com>
2025-03-09 18:02:05 +0100

    Implement clean-up thread

---
+++ in4labs_perception_app/config.py
@@ -0,0 +1,9 @@
+# Boards configuration
+boards_config = {
+    'Board_1':{
+        'name':'Mega 2560',
+        'model':'Arduino Mega 2560 Rev3',
+        'fqbn':'arduino:avr:mega',
+        'usb_port':'1',
+    }
+}
